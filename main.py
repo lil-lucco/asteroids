5207cc612477ac294a4eebe96b339e776f37a7b6
lil-lucco <lucas.tran117@gmail.com>
2026-01-21 15:07:48 -0500

    circle class added

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import pygame # type: ignore
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 def main():
-    pygame.init()
-    state = 1
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     print("Starting Asteroids with pygame version: " + pygame.version.ver)
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    while state != 0:
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +19,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60)
 
 if __name__ == "__main__":
     main()
