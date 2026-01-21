03c5e9812eaab588f0bf37f715b209edcd984b55
lil-lucco <lucas.tran117@gmail.com>
2026-01-21 18:33:22 -0500

    ch3l4

--- player.py
+++ player.py
@@ -26,7 +26,16 @@ class Player(CircleShape):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            neg_dt = -1 * dt
-            self.rotate(neg_dt)
+            self.rotate(-1 * dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-1 * dt)
+
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)  
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector        
